61954826a49fff22294f9608c60e4fd3b5664807
Francesc Campoy <campoy@golang.org>
2018-12-07 16:26:23 -0800

    day one part one
    Signed-off-by: Francesc Campoy <campoy@golang.org>

---
+++ day-01-p-1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	sum := 0
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		var n int
+		_, err := fmt.Sscanf(s.Text(), "%d", &n)
+		if err != nil {
+			log.Fatalf("could not read %s: %v", s.Text(), err)
+		}
+		sum += n
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sum)
+}
