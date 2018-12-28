f1203154d2fd128669d1149708a847438789fc0b
Francesc Campoy <campoy@golang.org>
2018-12-28 10:13:53 +0100

    updated skeleton to new style

--- skeleton.go
+++ skeleton.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	f, err := os.Open("input.txt")
+	_, err := readInput("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type data struct{}
+
+func readInput(path string) (*data, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not open %s: %v", path, err)
+	}
 	defer f.Close()
+
+	var d data
 
 	s := bufio.NewScanner(f)
 	for s.Scan() {
+		// do stuff
 	}
-	if err := s.Err(); err != nil {
-		log.Fatal(err)
-	}
+	return &d, s.Err()
 }
